96f228ab83c560dfe2debef2de92dc99da7ce29f
Gennaro Tortone <gennaro.tortone@na.infn.it>
2025-03-07 13:29:38 +0100

    integration of VXM in DeviceCollection

--- lib/DeviceCollection.py
+++ lib/DeviceCollection.py
@@ -3,15 +3,15 @@ import sys
 import serial
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from lib.RPC import RPCDevice
+from lib.VXM import VXM
 
 class DeviceCollection:
     def __init__(self):
-        self.outlets = {}
         self.serials = {}
+        self.outlets = {}
+        self.motors = {}
 
     def add_outlet(self, id, name, port, speed=115200, bytesize=8, parity='N', stopbits=1, timeout=1):
-        # create here serial object and track for same port usage
-        # from different channels
         if(self.serials.get(port, None) == None):
             s = serial.Serial(
                     port=port, 
@@ -27,6 +27,24 @@ class DeviceCollection:
 
     def get_outlet(self, name):
         return self.outlets[name]
+
+    def add_motor(self, id, name, port, speed=115200, bytesize=8, parity='N', stopbits=1, timeout=1):
+        if(self.serials.get(port, None) == None):
+            s = serial.Serial(
+                    port=port, 
+                    baudrate=speed, 
+                    bytesize=bytesize, 
+                    parity=parity,
+                    stopbits = stopbits,
+                    timeout = timeout)
+            self.serials[port] = VXM(s)
+
+        vxm = self.serials[port]
+        self.motors[name] = vxm.add_motor(id, name)
+
+    def get_motor(self, name):
+        return self.motors[name]
+
 
     def __repr__(self):
         return f'{self.outlets}'
